e82f52c809d1cd87d184a12ba61fc0a4ad10dd57
Vincent Le <vincent.tn.le@gmail.com>
2022-07-18 18:20:10 -0400

    Completed game functionality Game finishes when one player reaches a score of 5

--- script.js
+++ script.js
@@ -3,74 +3,39 @@ function computerPlay() {
     return options[Math.floor(Math.random() * 3)];
 }
 
-function playerPlay() {
-    let playerChoice = window.prompt("What's your play? ").toLowerCase();
-    return playerChoice;
-}
-
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" & computerSelection == "paper") {
+    if (playerScore == 5 | computerScore == 5) {
+        return;
+    }
+    else if (playerSelection == "rock" & computerSelection == "paper") {
         return "You lose! Paper beats Rock"
     }
     else if (playerSelection == "rock" & computerSelection == "scissors") {
         return "You win! Rock beats Scissors"
     }
-
     else if (playerSelection == "paper" & computerSelection == "rock") {
         return "You win! Paper beats Rock"
     }
     else if (playerSelection == "paper" & computerSelection == "scissors") {
         return "You lose! Scissors beats Paper"
     }
-
-
     else if (playerSelection == "scissors" & computerSelection == "rock") {
         return "You lose! Rock beats Scissors"
     }
     else if (playerSelection == "scissors" & computerSelection == "paper") {
         return "You win! Scissors beats Paper"
     }
-
     else if (playerSelection == computerSelection) {
         return "Tie Game"
     }
 }
 
-function game() {
-    let playerScore=0;
-    let computerScore=0;
 
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = playerPlay().toLowerCase()
-        let computerChoice = computerPlay().toLowerCase();
-        let result = playRound(playerChoice, computerChoice);
-        
-        if (result.includes("win")) {
-            playerScore++;
-            console.log(`Player wins round ${i+1}`)
-        }
-        else if (result.includes("lose")) {
-            computerScore++;
-            console.log(`Computer wins round ${i+1}`)
-        }
-        else {
-            console.log(`Round ${i+1} is a tie. Repeat`)
-            i--;
-        }
-        console.log(`Player Score: ${playerScore}\nComputer Score: ${computerScore}`)
-    }
-
-    if (playerScore > computerScore) {
-        console.log("Player wins!");
-    }
-
-    else if (playerScore < computerScore) {
-        console.log("Computer wins!");
-    }
-}
-
-/*
-*/
+// Setting the initial scores
+let playerScore = 0;
+let computerScore = 0;
+document.getElementById("player-score").innerHTML = `Player Score: ${playerScore}`;
+document.getElementById("computer-score").innerHTML = `Computer Score: ${computerScore}`;
 
 const buttons = document.querySelectorAll('button');
 
@@ -78,5 +43,26 @@ buttons.forEach((button) => {
     button.addEventListener('click', () => {
         const result = playRound(button.className, computerPlay())
         console.log(result);
+        // After each round, result of the round will be displayed and score will be updated
+        if (result.includes("win")) {
+            playerScore++;
+            document.getElementById("round-result").innerHTML = result;
+            document.getElementById("player-score").innerHTML = `Player Score: ${playerScore}`;
+        }
+        else if (result.includes("lose")) {
+            computerScore++;
+            document.getElementById("round-result").innerHTML = result;
+            document.getElementById("computer-score").innerHTML = `Computer Score: ${computerScore}`;
+        }
+        else {
+            document.getElementById("round-result").innerHTML = result;
+        }
+
+        if (playerScore == 5) {
+            document.getElementById("round-result").innerHTML = "Player has won! :)";
+        }
+        else if (computerScore == 5) {
+            document.getElementById("round-result").innerHTML = "Computer has won! :(";
+        }
     })
-})
+});
